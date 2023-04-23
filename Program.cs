5faa2ce0ac30fa1cc271fdfdd94e687022d6e30c
«Vyacheslav» <>
2023-04-23 13:25:22 +0300

    Task 1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿Console.WriteLine("Введите число a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число b");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a > b)
+{
+    Console.WriteLine($"max = {a}, min = {b}");
+}
+Console.WriteLine($"max = {b}, min = {a}");
